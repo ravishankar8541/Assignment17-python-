4735676ac47a2260181db856f5138b7d50a2f63f
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-10-23 08:19:24 +0530

    Add files via upload

---
+++ Q4.py
@@ -0,0 +1,7 @@
+"""Write a Python script to find if “Python” is present in the set thisset = {"Java","Python", "Django"}"""
+a="Python"
+thisset = {"Java","Python", "Django"}
+print(a in thisset)
+
+
+
