2efca812c7e13fbdfe179209fe2951ced1fc1b7b
ravishankar8541 <107765864+ravishankar8541@users.noreply.github.com>
2022-10-23 08:17:27 +0530

    Add files via upload

---
+++ Q2.py
@@ -0,0 +1,7 @@
+#2. Write a python program to store your own information {name, age, gender, so on..}
+s1={"Ravi Shankar Kumar Ray"}
+print("Enter the age,gender,ph-no,class,:")
+for a in range(1,5,1):
+    s=eval(input())
+    s1.add(s)
+print(s1)
